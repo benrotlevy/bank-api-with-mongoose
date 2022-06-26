5afd383ceda3fb37499e6999cb3d9351779802d8
benrotlevy <benjamin@rotlevy.com>
2022-06-26 10:24:09 +0300

    ready

--- server/app.js
+++ server/app.js
@@ -2,8 +2,14 @@ import express from "express";
 import { usersRoute } from "./routes/users.routes.js";
 import cors from "cors";
 import { manage } from "./routes/manage.routes.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const publicDirectoryPath = path.join(__dirname, "client/build");
+app.use(express.static(publicDirectoryPath));
 
 app.use(express.json());
 app.use(cors());
