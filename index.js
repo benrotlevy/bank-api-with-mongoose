bbcd9570afe8f9f270defa3b4af35d4a248caeb1
benrotlevy <benjamin@rotlevy.com>
2022-06-26 10:37:12 +0300

    ready

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import { app } from "./server/app.js";
 import "./server/db/mongoose.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const publicDirectoryPath = path.join(__dirname, "client/build");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(publicDirectoryPath));
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`);
--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 import express from "express";
 import { usersRoute } from "./routes/users.routes.js";
-import cors from "cors";
+// import cors from "cors";
 import { manage } from "./routes/manage.routes.js";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+// import path, { dirname } from "path";
+// import { fileURLToPath } from "url";
 
 const app = express();
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const publicDirectoryPath = path.join(__dirname, "client/build");
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+// const publicDirectoryPath = path.join(__dirname, "client/build");
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static(publicDirectoryPath));
+// app.use(express.json());
+// app.use(cors());
+// app.use(express.static(publicDirectoryPath));
 app.use("/users", usersRoute);
 app.use("/manage", manage);
 
