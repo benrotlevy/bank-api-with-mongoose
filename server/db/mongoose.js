b0a360742f9ffb8f82138ef113bfeafc991ee3f0
benrotlevy <benjamin@rotlevy.com>
2022-06-29 02:47:53 +0300

    complete

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,14 +5,15 @@ const connection = process.env.MONGOOSE_URI;
 
 mongoose.connect(
   connection,
-  {
-    autoIndex: true,
-  },
   (error, mongoConnectionInstance) => {
     if (error) throw Error("Mongoose Connection!!, Error: " + error);
     if (!process.env.NODE_ENV) {
       const { host, port, name } = mongoConnectionInstance;
+      console.log(mongoConnectionInstance);
       console.log({ host, port, name });
     }
+  },
+  {
+    autoIndex: true,
   }
 );
